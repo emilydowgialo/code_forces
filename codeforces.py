61237aef664374198756678bf6804dc47dcc0125
Emily Dowgialo <emilydowgialo@Emilys-MacBook-Air.local>
2016-07-07 13:50:42 -0700

    working on code challenges

--- codeforces.py
+++ codeforces.py
@@ -38,3 +38,50 @@ def side_measurer(side, a):
         num_tiles += 1
 
     return num_tiles
+
+
+# http://codeforces.com/problemset/problem/71/A
+def too_long_words(word):
+    """ If word is longer than 10 letters return the first and last letters of
+    the word with the number of letters between the first and last between those letters
+
+    >>> too_long_words("localization")
+    l10n
+
+    >>> too_long_words("word")
+    word
+    """
+
+    if len(word) > 10:
+        print word[0] + str(len(word[1:-1])) + word[-1]
+
+    else:
+        print word
+
+
+# http://codeforces.com/problemset/problem/4/A
+def watermelon(weight):
+    """ Check if a watermelon can be divided into 2 even parts
+
+    >>> watermelon(8)
+    'YES'
+
+    >>> watermelon(2)
+    'NO'
+
+    """
+
+    # 2 is the only even number that does not work
+    if weight == 2:
+        return "NO"
+
+    if (weight % 2) == 0:
+        return "YES"
+
+    else:
+        return "NO"
+
+if __name__ == '__main__':
+    import doctest
+    if doctest.testmod().failed == 0:
+        print "\n*** ALL TESTS PASSED. YOU ROCK!\n"
